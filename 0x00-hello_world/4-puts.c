e0c18f3ec2d8709850e91030939f0168ceb904b0
hussain <hussain4me2008@gmail.com>
2023-06-15 17:13:20 -0700

    Puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main A program that print a line with put function
+ *
+ * Return : 0 (success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
