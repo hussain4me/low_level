a0f95f3811934456a6f90fde9fb28dc7ac935759
hussain <hussain4me2008@gmail.com>
2023-06-21 15:46:36 -0700

    Sign checking commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include  "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: check value
+ *
+ * Return: 1 (greater than zero) 0 (equal zero) -1 (less than zero)
+ */
+
+int print_sign(int n)
+{
+	if ( n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
