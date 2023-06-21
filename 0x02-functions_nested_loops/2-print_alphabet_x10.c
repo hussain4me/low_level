930724e176617390adb509127a7f65c327299678
hussain <hussain4me2008@gmail.com>
2023-06-21 14:29:02 -0700

    Alphabet x10 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet, 
+ * in lowercase, followed by a new line.
+ *
+ * Return: No return
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char letter;
+
+
+	for (n = 0; n < 10; n++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+	}
+
+}
