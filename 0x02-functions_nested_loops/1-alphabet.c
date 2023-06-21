ef6e9923f76be9ce580408aaf375db29a0b2dbef
hussain <hussain4me2008@gmail.com>
2023-06-21 14:10:24 -0700

    Update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet -  a function that prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *  followed by a new line
  *
  * Return: No return
  */
