1d76c03b43483ff6ed92b6888c6ce1f2747adc21
hussain <hussain4me2008@gmail.com>
2023-06-22 15:43:07 -0700

    Commit all files

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an inifinite loop
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incomming: (\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i)*/
+	/*}*/
+	
+	printf("Infinite loop avoided \\o/\n");
+
+	return (0);
+}
