25710b855f4ee4a4fda1dbaf3a162f03d2f84aa4
hussain <hussain4me2008@gmail.com>
2023-06-21 18:16:34 -0700

    Add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @n1: first number
+ * @n2: Second number
+ *
+ * Return:numbers
+ */
+
+int add(int n1, int n2)
+{
+return n1 + n2;
+}
