c3d9279b1656770eb6ce0393c1220e2c3098689f
hussain <hussain4me2008@gmail.com>
2023-06-29 16:01:52 -0700

    Update files

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,18 +10,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
-		i++;
+		l++;
 	}
-	for (; x < i; x++)
+	for (; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest[i] = '\0';
+	dest[l] = '\0';
 	 
 	return (dest);
 }
