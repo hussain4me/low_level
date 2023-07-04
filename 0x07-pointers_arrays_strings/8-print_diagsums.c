4ec8abe49ab22c845326e8ba117e176a5937393f
hussain <hussain4me2008@gmail.com>
2023-07-04 07:55:20 -0700

    Update commit files

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * set_string - a function that sets the value of
+ * a pointer to a char
+ * @s: input
+ * @to: input
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+int sum1, sum2, y;
+
+sum1 = 0;
+sum2 = 0;
+
+for (y = 0; y < size; y++)
+{
+sum1 = sum1 + a[y * size + y];
+}
+for (y = size - 1; y >= 0; y--)
+{
+sum2 += a[y * size + (size - y - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
